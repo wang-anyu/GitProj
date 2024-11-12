b0439860b6ab2a1d22172f28aa4f3245bc0d75d6
ВАН Шанни <ANYU.W@dune.edu>
2024-11-12 15:57:59 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/',(req,res) => {
-    res.send("Hello World!");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.listen(port,() => {
-    console.log('Server is running at http://localhost:${port}');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
